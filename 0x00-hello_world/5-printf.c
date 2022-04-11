608f8c06bdf3a7cc342ad8923ef430b84097e2ff
NaheemOkunade <naim.okunade@gmail.com>
2022-04-11 12:18:02 +0000

    Task 5, Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 /**
  * Task 5
- * 
- * I am to write a code that prints out with proper grammar, but the outcome is a piece of art,
+ *
+ * I am to write a code that prints out:
+ * "with proper grammar, but the outcome is a piece of art,"
+ *
  * Using the printf function
  * And also compile using the gcc option -Wall
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
-	return(0);
+	printf("\nwith proper grammar, but the outcome is a piece of art,");
+	return (0);
 }
