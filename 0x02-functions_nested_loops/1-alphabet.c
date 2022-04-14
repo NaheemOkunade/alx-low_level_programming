5445cd48091f6a513ada989823d865da6c2a1b17
NaheemOkunade <naim.okunade@gmail.com>
2022-04-14 04:08:49 +0000

    Task 1 - 6

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,10 +6,9 @@
  */
 void print_alphabet(void)
 {
-
 	char alphabet;
 
-	for (alphabet = 'a'; alphabet<= 'z'; alphabet++);
+	for (alphabet = 'a'; alphabet<= 'z'; alphabet++)
 	{
 		_putchar(alphabet);
 	}
