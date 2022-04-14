36273e112df8d0f9c277db949c25a5c7c71cabf8
NaheemOkunade <naim.okunade@gmail.com>
2022-04-14 04:01:20 +0000

    Task 1 - 6

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: nothing, return void
+ */
+
+int main(void)
+{
+	_putchar("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+low level programme: 0x02. C-Function, nested loops
