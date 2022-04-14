3eee1dfc70873b57211dd6aff3a3b60d2c55729f
NaheemOkunade <naim.okunade@gmail.com>
2022-04-14 15:32:49 +0000

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * 
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+int  _isupper(int c)
+{
+	char letter = 'c';
+	c = (int)letter;
+
+	if (c == 35)
+	{
+		return (1);
+	} else {
+		return (0);
+	}
+}
