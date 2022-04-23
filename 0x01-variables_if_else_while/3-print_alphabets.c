c050d09affe919769fdb606f8fc51223c5dfbdfd
NaheemOkunade <naim.okunade@gmail.com>
2022-04-23 22:29:23 +0000

    Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main = Entry point
- * 
+ * main - Entry point
+ *
  * Return: Always 0 (success)
  */
 int main(void)
@@ -9,16 +9,16 @@ int main(void)
 	char up = 'A';
 	char low = 'a';
 
-	while (up <= 'Z')
-	{
-		putchar(up);
-		up++;
-	}
-
 	while (low <= 'z')
 	{
 		putchar(low);
 		low++;
+	}
+
+	while (up <= 'Z')
+	{
+		putchar(up);
+		up++;
 	}
 
 	putchar('\n');
