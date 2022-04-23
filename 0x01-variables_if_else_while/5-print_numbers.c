08d8332aa1101eaf39a69a2b1a3a1e11aa742041
NaheemOkunade <naim.okunade@gmail.com>
2022-04-23 22:50:07 +0000

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main (void)
+{
+	char singles;
+	for (singles = 0; single <= 9; singles++)
+	{
+		putchar(singles);
+	}
+	putchar('\n');
+	return (0);
+}
