c896c6cfb9473324f972267d0000a7bb767b224d
NaheemOkunade <naim.okunade@gmail.com>
2022-04-24 13:49:07 +0000

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,17 @@
 #include "main.h"
-
 /**
- * print_alphabet - print alphabet lowercase
+ * main - Entry point
  *
- * Return: Always 0.
+ * Return: Always 0 (success)
  */
-
 void print_alphabet(void)
 {
-char alphabet;
+	char alphabet;
 
-for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-{
-_putchar(alphabet);
-}
-
-_putchar('\n');
-
+	for (alphabet =  'a'; alphabet <= 'z'; alphabet++)
+	{
+		_putchar(alphabet);
+	}
+	_putchar('\n');
+	return (0);
 }
