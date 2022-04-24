7953fae1c730ec2d16dda1b76c6a7c4368b1c8aa
NaheemOkunade <naim.okunade@gmail.com>
2022-04-24 13:31:01 +0000

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,19 @@
 #include "main.h"
-
-
 /**
  * main - Entry point
  *
- * Return: nothing, return void
+ * Return: Always 0 (success)
  */
-
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 
-return (0);
+	return (0);
 }
